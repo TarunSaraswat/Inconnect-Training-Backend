a8202cc7a874c86c0c85af167384f60dec4dee04
TarunSaraswat <tarun.saraswat@innovaccer.com>
2022-02-10 13:25:42 +0530

    fix: Meetviewset create

--- Appointment/views.py
+++ Appointment/views.py
@@ -1,27 +1,20 @@
-from datetime import datetime
-from Appointment.filters import MeetFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import UpdateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.http import HttpResponse
 from Appointment.filters import MeetFilter
 from User.models import Doctors, Patients
-from .models import DoctorService, Meet
-from .serializers import MeetSerializer
+from Appointment.models import DoctorService, Meet
+from Appointment.serializers import MeetSerializer
 
 
 class AddRating(GenericAPIView, UpdateModelMixin):
     queryset = Meet.objects.all().filter(done=True)
     serializer_class = MeetSerializer
     permission_classes = [IsAuthenticated]
-
-    def put(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
 
 
 class MeetViewSet(viewsets.ModelViewSet):
@@ -37,63 +30,43 @@ class MeetViewSet(viewsets.ModelViewSet):
         return Response(doc)
 
     def create(self, request, *args, **kwargs):
-        data = MeetSerializer(data=request.data)
-        if data.is_valid():
-            start_datetime = data['start_datetime'].value
-            end_datetime = data['end_datetime'].value
-            start_datetime = datetime.strptime(start_datetime, "%Y-%m-%dt%H:%M:%S")
-            end_datetime = datetime.strptime(end_datetime, "%Y-%m-%dt%H:%M:%S")
-            if start_datetime > end_datetime:
-                return Response(status=400)
-            patient_id = data['patient_id'].value
-            service = data['service'].value
-            patient = Patients.objects.get(id=patient_id)
-            doctor_at_zipcode = Doctors.objects.filter(zipcode=patient.zipcode).values_list('id', flat=True)
-            doctor_service = DoctorService.objects.filter(service=service).values_list('id', flat=True)
-            doctor = []
-            for docZipcode in doctor_at_zipcode:
-                print(docZipcode)
-                for docService in doctor_service:
-                    print(docService)
-                    if docZipcode == docService:
-                        doctor.append(docZipcode)
-            if not doctor:
-                return Response(status=404)
+        serializer = MeetSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        patient = Patients.objects.get(id=data.get('patient_id').id)
+        doctor_at_zipcode = Doctors.objects.filter(zipcode=patient.zipcode).values_list('id', flat=True)
+        doctor_service = DoctorService.objects.filter(service=data.get('service')).values_list('id', flat=True)
+        doctor = [
+            doctor_zipcode
+            for doctor_zipcode in doctor_at_zipcode
+            if doctor_zipcode in doctor_service
+        ]
+        assigned_doctor = None
+        for d in doctor:
+            booking_reject = False
+            doc_meet = Meet.objects.filter(assigned_doctor=d)
 
-            bookings = []
-            for d in doctor:
-                booking_reject = False
-                doc_meet = Meet.objects.filter(assigned_doctor=d)
-
-                for meet in doc_meet:
-                    if meet.end_datetime < start_datetime:
-                        continue
-                    elif meet.start_datetime > end_datetime:
-                        continue
-                    else:
-                        booking_reject = True
-                        break
-                if booking_reject:
+            for meet in doc_meet:
+                if meet.end_datetime < data.get('start_datetime'):
+                    continue
+                elif meet.start_datetime > data.get('end_datetime'):
                     continue
                 else:
-                    bookings.append(Doctors.objects.get(id=d))
+                    booking_reject = True
                     break
-
-            if not bookings:
-                return Response(status=404)
-
+            if booking_reject:
+                continue
             else:
-                assigned_doctor = bookings[0]
-
-            meet = Meet.objects.create(
-                patient_id=patient, start_datetime=start_datetime, end_datetime=end_datetime,
-                assigned_doctor=assigned_doctor, service=service)
-            meet.save()
-
+                assigned_doctor = Doctors.objects.get(id=d)
+                break
+        if assigned_doctor:
+            meet_serializer = Meet.objects.create(
+                patient_id=patient, start_datetime=data.get('start_datetime'), end_datetime=data.get('end_datetime'),
+                assigned_doctor=assigned_doctor, service=data.get('service'))
+            meet_serializer.save()
             return Response(status=201)
-
         else:
-            return Response(status=400)
+            return Response(status=404, data={'issue': 'no free timeslot found for requested service'})
 
 
 class GetByZipcode(APIView):
--- User/test_models.py
+++ User/test_models.py
@@ -1,59 +1,67 @@
-from django.test import TestCase
 import unittest
+from django.test import TestCase
 from User.models import Doctors, Patients
 
+
 class BaseModelTestCase(TestCase):
-        
+
     @classmethod
     def setUpClass(cls):
         super(BaseModelTestCase, cls).setUpClass()
-        cls.doctor_one = Doctors(email="Saqnjeev@gmail.com", name="Sanjeev Rathore", city= "Lucknow",	state= "Uttar Pradesh",	zipcode= "204101")
+        cls.doctor_one = Doctors(email="Saqnjeev@gmail.com", name="Sanjeev Rathore", city="Lucknow",
+                                 state="Uttar Pradesh", zipcode="204101")
         cls.doctor_one.save()
-        cls.doctor_two = Doctors(email="tarun@gmail.com", name="Tarun", city= "Lucknow", state= "Uttar Pradesh", zipcode= "204101")
+        cls.doctor_two = Doctors(email="tarun@gmail.com", name="Tarun", city="Lucknow", state="Uttar Pradesh",
+                                 zipcode="204101")
         cls.doctor_two.save()
-        cls.patient_one = Patients(email="tarun@gmail.com", name="Tarun", city= "Lucknow",	state= "Uttar Pradesh",	zipcode= "204101")
+        cls.patient_one = Patients(email="tarun@gmail.com", name="Tarun", city="Lucknow", state="Uttar Pradesh",
+                                   zipcode="204101")
         cls.patient_one.save()
-        cls.patient_two = Patients(email="Saqnjeev@gmail.com", name="Sanjeev Rathore", city= "Lucknow",	state= "Uttar Pradesh",	zipcode= "204101")
+        cls.patient_two = Patients(email="Saqnjeev@gmail.com", name="Sanjeev Rathore", city="Lucknow",
+                                   state="Uttar Pradesh", zipcode="204101")
         cls.patient_two.save()
-        
+
+
 class PatientModelTestCase(BaseModelTestCase):
     def test_patient_one_created_properly(self):
         self.assertEqual(True, self.patient_one in Patients.objects.all())
-        
+
     def test_patient_two_created_properly(self):
         self.assertEqual(True, self.patient_two in Patients.objects.all())
-        
-        
+
+
 class DoctorModelTestCase(BaseModelTestCase):
     def test_doctor_one_created_properly(self):
         self.assertEqual(True, self.doctor_one in Doctors.objects.all())
-        
+
     def test_doctor_two_created_properly(self):
         self.assertEqual(True, self.doctor_two in Doctors.objects.all())
-        
+
+
 class PatientFailModelTestCase(TestCase):
     @unittest.expectedFailure
     def test_fail_patient_insertion_one(self):
-        "giving only one field in patients"
-        patient_fail=Patients(email="Saqnjeev@gmail.com")
+        """giving only one field in patients"""
+        patient_fail = Patients(email="Saqnjeev@gmail.com")
         patient_fail.save()
 
     @unittest.expectedFailure
     def test_fail_patient_insertion_two(self):
         "zipcode field missing in patients object"
-        patient_fail=Patients(email="Saqnjeev@gmail.com", name="Sanjeev Rathore", city= "Lucknow",	state= "Uttar Pradesh")
+        patient_fail = Patients(email="Saqnjeev@gmail.com", name="Sanjeev Rathore", city="Lucknow",
+                                state="Uttar Pradesh")
         patient_fail.save()
 
 
 class DoctorFailModelTestCase(TestCase):
     @unittest.expectedFailure
     def test_fail_doctor_insertion_one(self):
-        "giving only one field in doctors"
-        doctor_fail=Doctors(email="Saqnjeev@gmail.com")
+        """giving only one field in doctors"""
+        doctor_fail = Doctors(email="Saqnjeev@gmail.com")
         doctor_fail.save()
 
     @unittest.expectedFailure
     def test_fail_doctor_insertion_two(self):
-        "zipcode field missing in doctors object"
-        doctor_fail=Doctors(email="Saqnjeev@gmail.com", name="Sanjeev Rathore", city= "Lucknow",	state= "Uttar Pradesh")
+        """zipcode field missing in doctors object"""
+        doctor_fail = Doctors(email="Saqnjeev@gmail.com", name="Sanjeev Rathore", city="Lucknow", state="Uttar Pradesh")
         doctor_fail.save()
